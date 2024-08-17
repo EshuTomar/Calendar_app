8471abfe66d9eeb119d885eba6779c0d2aeab98f
Eshu tomar <eshutomar1@gmial.com>
2024-08-17 15:27:36 +0530

    Setup done

---
+++ Calendar_app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calendar from './components/Calendar'
+import './components/Calendar.css'
+
+const App = () => {
+  return (
+    <div>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
