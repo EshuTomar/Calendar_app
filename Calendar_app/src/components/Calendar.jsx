5ae5c8c67a83952b1e504b13357d557cf5ff3cac
Eshu tomar <eshutomar1@gmial.com>
2024-08-20 16:48:09 +0530

    calendar section done

--- Calendar_app/src/components/Calendar.jsx
+++ Calendar_app/src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCaretLeft } from "react-icons/fa";
 import { FaCaretRight } from "react-icons/fa";
 import { IoMdCloseCircleOutline } from "react-icons/io";
@@ -6,31 +6,57 @@ import { MdModeEditOutline } from "react-icons/md";
 import { RiMessage2Fill } from "react-icons/ri";
 
 const Calendar = () => {
+
+  const daysInWeek =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ]
+  const months = ["January", "February", "March","April", "May","June","July","August","September","October","November","December"]
+
+  const currDate = new Date()
+  const [currMonth, setCurrMonth] = useState(currDate.getMonth());
+  const [currYear, setCurrYear] = useState(currDate.getFullYear());
+
+  const daysInMonth = new Date(currYear, currMonth+1, 0).getDate();
+
+  const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+  
+  // console.log(currDate, currMonth, currYear);
+  // console.log(daysInMonth, firstDayOfMonth)
+
+  const prevMonth = () =>{
+    setCurrMonth((prevMonth)=> (prevMonth === 0 ? 11 : prevMonth - 1))
+    setCurrYear((prevYear)=> (currMonth === 0 ? prevYear - 1 : prevYear))
+  }
+  const nextMonth = () =>{
+    setCurrMonth((prevMonth)=> (prevMonth === 11 ? 0 : prevMonth + 1))
+    setCurrYear((prevYear)=> (currMonth === 11 ? prevYear + 1 : prevYear))
+  }
+
   return (
     <div className='container'>
     <div className='calendar-app'>
       <div className='calendar'>
         <h1 className='heading'>Calendar</h1>
         <div className='navigate-date'>
-            <h2 className='month'>August,</h2>
-            <h2 className='year'> 2024</h2>
+            <h2 className='month'>{months[currMonth]} </h2>
+            <h2 className='year'> {currYear} </h2>
             <div className='button'>
-                <FaCaretLeft className='icon'/>
-                <FaCaretRight className='icon' />
+                <FaCaretLeft className='icon' onClick={prevMonth}/>
+                <FaCaretRight className='icon' onClick={nextMonth}/>
             </div>
             
         </div>
         <div className='weekdays'>
-            <span>Mon</span>
+            {/* <span>Mon</span>
             <span>Tue</span>
             <span>Wed</span>
             <span>Thu</span>
             <span>Fri</span>
             <span>Sat</span>
-            <span>Sun</span>
+            <span>Sun</span> */}
+            {daysInWeek.map((day)=>
+              <span key={day}>{day}</span>)}
         </div>
         <div className='days'>
-            <span>1</span>
+            {/* <span>1</span>
             <span>2</span>
             <span>3</span>
             <span>4</span>
@@ -60,14 +86,19 @@ const Calendar = () => {
             <span>28</span>
             <span>29</span>
             <span>30</span>
-            <span>31</span>
+            <span>31</span> */}
+            {[...Array(firstDayOfMonth).keys()].map((_, index)=>
+            <span key={`empty-${index}`} />
+            )}
+            {[...Array(daysInMonth).keys()].map((day)=>
+            <span key={day+1} className={day+1 === currDate.getDate() && currMonth === currDate.getMonth() && currYear === currDate.getFullYear() ? 'curr-date' : ""}>{day+1}</span>)}
             
          </div>
       </div>
       <div className='events'>
           <div className='event-popup'>
             <div className='time-input'>
-              <div className='event-popup-time'>Time</div>
+              <div className='event-popup-time'>Time: </div>
                 <input type='number' name='hours' min={0} max={24} className='hours'/>
                 <input type='number' name='minutes' min={0} max={60} className='mins'/>
               </div>
